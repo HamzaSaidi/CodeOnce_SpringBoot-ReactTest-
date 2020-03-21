37a3409b31e4d9259df493df50b7f11e158ef43e
HAMZA <hsaidi291@gmail.com>
2020-03-21 04:31:16 +0100

    handle cors

--- backend/src/main/java/hamza/backend/Controller/Api.java
+++ backend/src/main/java/hamza/backend/Controller/Api.java
@@ -4,18 +4,14 @@ import hamza.backend.Service.MatchCount;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/api")
 public class Api {
 
     @Autowired
     MatchCount matchCount;
-
     @PostMapping(value = "/count")
     public ResponseEntity<Integer> CountMatch(@RequestBody String input) {
 
